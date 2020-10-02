6a01031b2741bc10d20ea046ce29f84036b33c84
hacker65536 <s.hacker65536@gmail.com>
2020-10-02 11:55:46 +0900

    fix

--- pkg/resource/cluster/resource.go
+++ pkg/resource/cluster/resource.go
@@ -172,6 +172,7 @@ func ResourceCluster() *schema.Resource {
 			KeyDrainNodeGroups: {
 				Type:     schema.TypeList,
 				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			resource.KeyOutput: {
 				Type:     schema.TypeString,
