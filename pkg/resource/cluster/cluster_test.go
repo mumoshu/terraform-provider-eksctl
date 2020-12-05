mumoshu/terraform-provider-eksctl

commit a8b085e59479f14a870033e1193fae4e07f2ea66

Run IAM related operations only when `iam.withOIDC` is true
This fixes a few errors and panic cases when `iam.withOIDC` is not set in recent versions of eksctl (perhaps 0.30.0 or greater)

pkg/resource/cluster/cluster.go
@@ -81,6 +81,16 @@ type Cluster struct {
 	Metrics          []courier.Metric
 }
 
+func (c Cluster) IAMWithOIDCEnabled() (bool, error) {
+	var config EksctlClusterConfig
+
+	if err := yaml.Unmarshal([]byte(c.Spec), &config); err != nil {
+		return false, fmt.Errorf("parsing cluster.yaml: %w\nCONTENT:\n%s", err, c.Spec)
+	}
+
+	return config.IAM.WithOIDC, nil
+}
+
 func (c Cluster) GitOpsEnabled() (bool, error) {
 	var config EksctlClusterConfig
 
@@ -111,6 +121,11 @@ type EksctlClusterConfig struct {
 	// on `git: {}` in the generated cluster.yaml.
 	Git  map[string]interface{} `yaml:"git,omitempty"`
 	Rest map[string]interface{} `yaml:",inline"`
+	IAM  IAM                    `yaml:"iam"`
+}
+
+type IAM struct {
+	WithOIDC bool `yaml:"withOIDC"`
 }
 
 type VPC struct {

pkg/resource/cluster/cluster_read.go
@@ -119,6 +119,13 @@ func (m *Manager) planCluster(d *DiffReadWrite) error {
 }
 
 func readIAMIdentityMapping(d ReadWrite, cluster *Cluster) error {
+	iamWithOIDCEnabled, err := cluster.IAMWithOIDCEnabled()
+	if err != nil {
+		return fmt.Errorf("reading iam.withOIDC setting from cluster.yaml: %w", err)
+	} else if !iamWithOIDCEnabled {
+		return nil
+	}
+
 	iams, err := runGetIAMIdentityMapping(cluster)
 	if err != nil {
 		return fmt.Errorf("can not get iamidentitymapping from eks cluster: %w", err)

pkg/resource/cluster/cluster_update.go
@@ -225,6 +225,19 @@ func (m *Manager) updateCluster(d *schema.ResourceData) error {
 
 	}
 
+	whenIAMWithOIDCEnabled := func(f func() error) func() error {
+		return func() error{
+			iamWithOIDCEnabled, err := cluster.IAMWithOIDCEnabled()
+			if err != nil {
+				return fmt.Errorf("reading iam.withOIDC setting from cluster.yaml: %w", err)
+			} else if !iamWithOIDCEnabled {
+				return nil
+			}
+
+			return f()
+		}
+	}
+
 	updateIAMIdentityMapping := func() func() error {
 		return func() error {
 			d.HasChange(KeyIAMIdentityMapping)
@@ -237,6 +250,7 @@ func (m *Manager) updateCluster(d *schema.ResourceData) error {
 			if err := runDeleteIAMIdentityMapping(a.(*schema.Set).Difference(b.(*schema.Set)), cluster); err != nil {
 				return fmt.Errorf("DeleteIAMIdentityMapping Error: %v", err)
 			}
+
 			return nil
 		}
 	}
@@ -248,14 +262,14 @@ func (m *Manager) updateCluster(d *schema.ResourceData) error {
 		updateBy([]string{"utils", "update-aws-node"}, nil),
 		updateBy([]string{"utils", "update-coredns"}, nil),
 		createNew("nodegroup", nil, nil),
-		associateIAMOIDCProvider(),
-		createNew("iamserviceaccount", []string{"--approve"}, nil),
+		whenIAMWithOIDCEnabled(associateIAMOIDCProvider()),
+		whenIAMWithOIDCEnabled(createNew("iamserviceaccount", []string{"--approve"}, nil)),
 		createNew("fargateprofile", nil, harmlessFargateProfileCreationErrors),
 		enableRepo(),
 		draineNodegroup(),
-		updateIAMIdentityMapping(),
+		whenIAMWithOIDCEnabled(updateIAMIdentityMapping()),
 		deleteMissing("nodegroup", []string{"--drain", "--approve"}, nil),
-		deleteMissing("iamserviceaccount", []string{"--approve"}, nil),
+		whenIAMWithOIDCEnabled(deleteMissing("iamserviceaccount", []string{"--approve"}, nil)),
 		// eksctl delete fargate profile doens't has --only-missing command
 		//deleteMissing("fargateprofile", nil, []string{"Error: invalid Fargate profile: empty name"}),
 		applyKubernetesManifests(id),

commit 103e2f68c76a7b9738cda9d0a87cc67a47c6e530

Fix missing serviceAccounts in generated eksctl config
Fixes #27

This bug was introduced by a8b085e59479f14a870033e1193fae4e07f2ea66, and has been there since terraform-eksctl-provider v0.11.2 and until v0.13.1.

pkg/resource/cluster/cluster.go
@@ -128,7 +128,8 @@ type EksctlClusterConfig struct {
 }
 
 type IAM struct {
-	WithOIDC bool `yaml:"withOIDC"`
+	WithOIDC bool                   `yaml:"withOIDC"`
+	Rest     map[string]interface{} `yaml:",inline"`
 }
 
 type VPC struct {
@@ -259,18 +260,7 @@ metadata:
 %s
 `, a.APIVersion, clusterName, a.Region, a.Version, tagsJson, specStr))
 
-	c := EksctlClusterConfig{
-		VPC: VPC{
-			ID: "",
-			Subnets: Subnets{
-				Public:  map[string]Subnet{},
-				Private: map[string]Subnet{},
-			},
-			Rest: map[string]interface{}{},
-		},
-		NodeGroups: []NodeGroup{},
-		Rest:       map[string]interface{}{},
-	}
+	c := clusterConfigNew()
 
 	if err := yaml.Unmarshal(seedClusterConfig, &c); err != nil {
 		return nil, fmt.Errorf("parsing generate cluster.yaml: %w: INPUT:\n%s", err, string(seedClusterConfig))
@@ -288,18 +278,9 @@ metadata:
 	//	}
 	//}
 
-	var mergedClusterConfig []byte
-	{
-		var buf bytes.Buffer
-
-		enc := yaml.NewEncoder(&buf)
-		enc.SetIndent(2)
-
-		if err := enc.Encode(c); err != nil {
-			return nil, err
-		}
-
-		mergedClusterConfig = buf.Bytes()
+	mergedClusterConfig, err := clusterConfigToYAML(c)
+	if err != nil {
+		return nil, err
 	}
 
 	log.Printf("seed cluster config:\n%s", string(seedClusterConfig))
@@ -328,6 +309,34 @@ metadata:
 			ClusterName:               string(clusterName),
 		},
 	}, nil
+}
+
+func clusterConfigToYAML(c EksctlClusterConfig) ([]byte, error) {
+	var buf bytes.Buffer
+
+	enc := yaml.NewEncoder(&buf)
+	enc.SetIndent(2)
+
+	if err := enc.Encode(c); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func clusterConfigNew() EksctlClusterConfig {
+	return EksctlClusterConfig{
+		VPC: VPC{
+			ID: "",
+			Subnets: Subnets{
+				Public:  map[string]Subnet{},
+				Private: map[string]Subnet{},
+			},
+			Rest: map[string]interface{}{},
+		},
+		NodeGroups: []NodeGroup{},
+		Rest:       map[string]interface{}{},
+	}
 }
 
 type ClusterName string

pkg/resource/cluster/cluster_test.go
@@ -1,0 +1,44 @@
+package cluster
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestConfig_MarshalUnmarshal(t *testing.T) {
+	input := `vpc:
+  id: ""
+  subnets:
+    public: {}
+    private: {}
+nodeGroups: []
+iam:
+  withOIDC: true
+  serviceAccounts:
+    - name: foo
+apiVersion: example.com/v1alpha1
+kind: ClusterConfig
+metadata:
+  name: mycluster
+  region: us-east-2
+  tags:
+    foo: bar
+  version: "1.18"
+`
+
+	c := clusterConfigNew()
+
+	if err := yaml.Unmarshal([]byte(input), &c); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	output, err := clusterConfigToYAML(c)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if d := cmp.Diff(string(input), string(output)); d != "" {
+		t.Errorf("unexpected diff: want (-), got (+)\n%s", d)
+	}
+}