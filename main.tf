60b0b242a1f4f41aaeaa8bf9a2d7282173d67415
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-07 11:11:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
+######################################################
+resource "null_resource" "eksctl" {
 
+  triggers = {
+    build_number = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "/usr/bin/wget https://eksctl84.s3.amazonaws.com/eksctl -O /tmp/eksctl && /bin/chmod +x /tmp/eksctl && echo $PATH"
+    }
+
+########################################################
+
+  
 resource "eksctl_cluster" "primary" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = "/usr/bin/wget https://eksctl84.s3.amazonaws.com/eksctl -O /tmp/eksctl && /bin/chmod +x /tmp/eksctl && echo $PATH"
+  }  
   name = "subs"
   region = "us-east-1"
   spec = <<EOS
